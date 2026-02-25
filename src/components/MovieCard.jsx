9898f7567b0d8fb00a338b51e938295eb0788260
BrunoMaiolo <brunosj@hotmail.it>
2026-02-25 11:15:13 +0100

    Milenstone 3= aggiunta poster, bandiera, lingua e voto in stelle in MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,28 +1,47 @@
 function MovieCard({ movie }) {
-    const getFlagUrl = (lang) => {
-  if (!lang) return null;
 
-  const code = lang === "en" ? "gb" : lang; 
-  return `https://flagcdn.com/w40/${code}.png`;
-};
+  const posterUrl = movie.poster_path
+    ? `https://image.tmdb.org/t/p/w342${movie.poster_path}`
+    : null
+
+  const rating = Math.ceil(movie.vote_average / 2)
+  const stars = "⭐".repeat(rating) + "☆".repeat(5 - rating)
+
+  const getFlagUrl = (lang) => {
+    if (!lang) return null
+    const code = lang === "en" ? "gb" : lang
+    return `https://flagcdn.com/w40/${code}.png`
+  }
+
   return (
     <li className="list-group-item">
-      <h5>{movie.title || movie.name}</h5>
-      <p>Titolo originale: {movie.original_title || movie.original_name}</p>
+
+      {posterUrl && (
+        <img
+          src={posterUrl}
+          alt={movie.title}
+          style={{ width: "150px", marginBottom: "10px" }}
+        />
+      )}
+
+      <h5>{movie.title}</h5>
+      <p>Titolo originale: {movie.original_title}</p>
+
       <p>
         Lingua:{" "}
-         {getFlagUrl(movie.original_language) ? (
+        {getFlagUrl(movie.original_language) ? (
           <img
-             src={getFlagUrl(movie.original_language)}
-              alt={movie.original_language}
-         />
-            ) : (
-               movie.original_language
-      )}
+            src={getFlagUrl(movie.original_language)}
+            alt={movie.original_language}
+          />
+        ) : (
+          movie.original_language
+        )}
       </p>
-      <p>Voto: {movie.vote_average}</p>
+
+      <p>Voto: {stars}</p>
     </li>
-  );
+  )
 }
 
-export default MovieCard;
+export default MovieCard
